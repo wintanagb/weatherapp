5c401aaa6fdc1b6ca073e65818139e689eae8060
wintanagb <wintanaltc@gmail.com>
2021-04-01 22:09:23 -0500

    class adjustments

---
+++ frontend/src/classes/dropdown.js
@@ -0,0 +1,54 @@
+class Dropdown{
+    constructor(){
+      this.dropDown = document.querySelector(".dropdown");
+    };
+    dropDownEvent(){
+        const dropdown = document.querySelector(".dropdown");
+        dropdown.addEventListener("click", () => dropdown.classList.toggle("is-active"));
+    }
+    populateDropDown(location){
+      const dropDown = document.querySelector(".dropdown-content");
+      const option = document.createElement("a");
+      option.className = "dropdown-item";
+      option.textContent = location.city;
+ 
+      dropDown.appendChild(option);
+ 
+      option.addEventListener("click", () => {
+          locationObject.addToLocationList(location);
+      });
+    }
+    createCityButton(location){
+      const container = document.querySelector(".flex-container-one");
+      const cityButton = document.createElement("button");
+      const deleteButton = document.createElement("button");
+ 
+      deleteButton.className = "button is-dark";
+      deleteButton.textContent = '❌';
+ 
+      cityButton.className = "button is-dark city-button";
+      cityButton.textContent = location.city;
+      cityButton.dataset.id =
+ 
+      cityButton.addEventListener("click", () => {
+          weatherObject.getWeatherConditions(location);
+      });
+ 
+      deleteButton.addEventListener("click", (e)=> {
+        const userLocationId = userObject.parseUserLocation(location);
+        e.target.parentElement.remove();
+        userObject.deleteCity(userLocationId) //persist change!
+      })
+ 
+      cityButton.appendChild(deleteButton);
+      container.appendChild(cityButton);
+ 
+      return deleteButton;
+    }
+    removeAllCityButtons(){
+      const cityButtons = document.querySelectorAll(".city-button");
+      cityButtons.forEach(button => button.remove());
+      // cityButtons.removeAll();
+ 
+    }
+  }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,3 +13,25 @@
 //       cities.array.forEach((city) => console.log(city))
 //   });
 // }
+
+let user;
+ // Creating objects of all the classes
+ let userObject;
+ let dropDownObject;
+ let locationObject;
+ let loginObject;
+ let weatherObject;
+
+ const URL = "http://localhost:3000"
+
+ document.addEventListener("DOMContentLoaded", () => {
+   userObject = new User();
+   dropDownObject = new Dropdown();
+   locationObject = new Location();
+   loginObject = new Login();
+   weatherObject = new Weather();
+   loginObject.renderModal();
+   loginObject.loginButtonEvent();
+   dropDownObject.dropDownEvent();
+   locationObject.fetchCities();
+ })
